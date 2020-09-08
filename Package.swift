c77accf5c309e6719295dfc4a4c3b2ae95909375
npatel05 <nirha.patel@verizonmedia.com>
2020-09-08 14:26:17 -0500

    rename

--- Package.swift
+++ Package.swift
@@ -32,26 +32,26 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FlurryAnalytics",
-            dependencies: ["FlurryAnalytics"]
+            dependencies: ["FlurryAnalyticsBinary"]
         ),
         .target(
             name: "FlurryConfig",
-            dependencies: ["FlurryConfig"]
+            dependencies: ["FlurryConfigBinary"]
         ),
         .target(
             name: "FlurryAds",
-            dependencies: ["FlurryAds"]
+            dependencies: ["FlurryAdsBinary"]
         ),
         .binaryTarget(
-            name: "FlurryAnalytics",
+            name: "FlurryAnalyticsBinary",
             path: "artifacts/Flurry.xcframework"
         ),
         .binaryTarget(
-            name: "FlurryConfig",
+            name: "FlurryConfigBinary",
             path: "artifacts/FlurryConfig.xcframework"
         ),
         .binaryTarget(
-            name: "FlurryAds",
+            name: "FlurryAdsBinary",
             path: "artifacts/FlurryAds.xcframework"
         ),
         
