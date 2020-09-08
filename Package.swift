0ff58cf551f9ca2c22c84e8fd25c27a76307af15
npatel05 <nirha.patel@verizonmedia.com>
2020-09-07 20:43:55 -0500

    trial

--- Package.swift
+++ Package.swift
@@ -14,10 +14,6 @@ let package = Package(
             name: "FlurryAnalyticsSPM",
             targets: ["FlurryAnalytics"]
         ),
-//        .library(
-//            name: "FlurryMessagingSPM",
-//            targets: ["FlurryMessaging"]
-//        ),
         .library(
             name: "FlurryConfigSPM",
             targets: ["FlurryConfig"]
@@ -74,36 +70,36 @@ let package = Package(
     ]
 )
 
-let packageMessaging = Package(
-    name: "FlurryMessaging",
-    platforms: [
-        .iOS("14")
-        ],
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "FlurryMessagingSPM",
-            targets: ["FlurryMessaging"]
-        ),
-    ],
-    dependencies: [
-            // Dependencies declare other packages that this package depends on.
-            // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "FlurryMessagingTarget",
-            dependencies: ["FlurryMessaging"]
-        ),
-        .binaryTarget(
-            name: "FlurryMessaging",
-            path: "artifacts/FlurryMessaging.xcframework"
-        ),
-        .testTarget(
-            name: "FlurryMessagingSPM",
-            dependencies: ["FlurryMessaging"]
-        ),
-    ]
-)
+//let packageMessaging = Package(
+//    name: "FlurryMessaging",
+//    platforms: [
+//        .iOS("14")
+//        ],
+//    products: [
+//        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+//        .library(
+//            name: "FlurryMessagingSPM",
+//            targets: ["FlurryMessaging"]
+//        ),
+//    ],
+//    dependencies: [
+//            // Dependencies declare other packages that this package depends on.
+//            // .package(url: /* package url */, from: "1.0.0"),
+//    ],
+//    targets: [
+//        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+//        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+//        .target(
+//            name: "FlurryMessagingTarget",
+//            dependencies: ["FlurryMessaging"]
+//        ),
+//        .binaryTarget(
+//            name: "FlurryMessaging",
+//            path: "artifacts/FlurryMessaging.xcframework"
+//        ),
+//        .testTarget(
+//            name: "FlurryMessagingSPM",
+//            dependencies: ["FlurryMessaging"]
+//        ),
+//    ]
+//)
